acbad83e119a60f71348cb6396ec1d60f2122262
YormanColinaMartinez <yormancolina30@gmail.com>
2024-06-15 10:54:49 -0500

    xcode proyect was created

---
+++ FreeStore/FreeStore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FreeStore
+//
+//  Created by Apple on 15/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
