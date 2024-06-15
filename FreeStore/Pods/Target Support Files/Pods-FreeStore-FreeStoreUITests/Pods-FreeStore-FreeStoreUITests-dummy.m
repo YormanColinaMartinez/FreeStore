92696401afb794774cf0482992954cf59fe06f64
YormanColinaMartinez <yormancolina30@gmail.com>
2024-06-15 10:57:49 -0500

    CocoaPods was added

---
+++ FreeStore/Pods/Target Support Files/Pods-FreeStore-FreeStoreUITests/Pods-FreeStore-FreeStoreUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FreeStore_FreeStoreUITests : NSObject
+@end
+@implementation PodsDummy_Pods_FreeStore_FreeStoreUITests
+@end
---
+++ FreeStore/Pods/Target Support Files/Pods-FreeStoreTests/Pods-FreeStoreTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FreeStoreTests : NSObject
+@end
+@implementation PodsDummy_Pods_FreeStoreTests
+@end
